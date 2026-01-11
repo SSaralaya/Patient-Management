9782b79e42497ad34e43821c8d74bd48626d1487
SSaralaya <shashanka0387@gmail.com>
2026-01-11 12:42:09 +0530

    Created ResponseDTO and maper classes

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,7 +1,14 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -10,4 +17,14 @@ public class PatientService {
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
     }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientResponseDTO> patientResponseDTOS = patients.stream()
+                .map(patient ->
+                    PatientMapper.toDTO(patient)
+                ).toList();
+        return patientResponseDTOS;
+    }
+
 }
